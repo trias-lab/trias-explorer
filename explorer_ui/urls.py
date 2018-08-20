55cd9b19a90bb79dea94072e8de479c44903fc7c
jiaxuechuan <jiaxuechuan@8lab.cn>
2018-08-20 02:26:20 -0700

    index data

--- explorer_ui/urls.py
+++ explorer_ui/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from app.views import (
+    index
+)
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', admin.site.urls),
+    url(r'^api/index_base_info/', index.index_base_info),  # Trias Blockchain BaseInfo
+    url(r'^api/index_latest_blocks/', index.index_latest_blocks),  # Latest Blocks 20 Numbers
+    url(r'^api/index_recent_transactions/', index.index_recent_transactions),  # Recent Transactions 20 Numbers
 ]
