783f6f075142e493ff5cd81d337f34aedd117552
Evelynzzz <15104026362@163.com>
2018-08-21 19:51:33 +0800

    Import sub navbar in block list page

--- html/components/BlockList.js
+++ html/components/BlockList.js
@@ -1,18 +1,29 @@
 import React from "react"
 import $ from 'jquery'
 import {injectIntl, intlShape, FormattedMessage } from 'react-intl'; /* react-intl imports */
+import SubNavbar from "./common/SubNavbar"
 
 export default class BlockList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            subNavbarMatch: this.props.match    // Route  match props
+        }
+    }
+
+    
+    componentWillReceiveProps(nextProps){
+        if(this.state.subNavbarMatch.url !== nextProps.match.url){
+            this.setState({
+                subNavbarMatch: nextProps.match
+            })
         }
     }
 
     render() {
         return (
             <div className="blocklist-container">
-            BlockList
+                <SubNavbar match={this.state.subNavbarMatch}/>
             </div>
         )
     }
