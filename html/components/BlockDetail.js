ae6a6ee0891f3e2fe74abf0f179077b64236c83e
Evelynzzz <15104026362@163.com>
2018-08-21 20:28:39 +0800

    Import sub navbar in block detail page

--- html/components/BlockDetail.js
+++ html/components/BlockDetail.js
@@ -1,19 +1,33 @@
 import React from "react"
 import $ from 'jquery'
 import {injectIntl, intlShape, FormattedMessage } from 'react-intl'; /* react-intl imports */
+import SubNavbar from "./common/SubNavbar"
 
 export default class BlockDetail extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            blockID: this.props.match.params.blockID
+            subNavbarMatch: this.props.match
+        }
+    }
+
+    /**
+     * Before a mounted component receives new props, reset some state.
+     * Determine whether the location is changed, then update the navigation bar based on the current URL.
+     * @param {Object} nextProps new props
+     */
+    componentWillReceiveProps(nextProps){
+        if(this.state.subNavbarMatch.url !== nextProps.match.url){
+            this.setState({
+                subNavbarMatch: nextProps.match
+            })
         }
     }
 
     render() {
         return (
             <div className="block-detail-container">
-            BlockDetail: {this.state.blockID}
+                <SubNavbar match={this.state.subNavbarMatch}/>                
             </div>
         )
     }
