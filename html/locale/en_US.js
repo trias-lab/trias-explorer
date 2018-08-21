e692d04bd5b6a3276c0f602df74886f1f67ba2aa
Evelynzzz <15104026362@163.com>
2018-08-21 19:48:44 +0800

    Add some translations

--- html/locale/en_US.js
+++ html/locale/en_US.js
@@ -1,6 +1,11 @@
 const en_US = {
-    home: "Homepage", 
+    home: "Home", 
     title: "Trias Blockchain Explorer",
+    block: "Block",
+    blocks: "Blocks",
+    transaction: "Transaction",
+    transactions: "Transactions",
+    address: "Address",
     email: "Email:",
     footerIntro:"TRIAS aims to define a new-generation all-platform-supported public chain system. Trustworthy and Reliable Intelligent Autonomous Systems make people trust in machines."
 }
