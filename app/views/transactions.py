e7d8b30863a79518c46a7ac9524221476bb65ae7
jiaxuechuan <jiaxuechuan@8lab.cn>
2018-08-28 01:32:57 -0700

    return blockHash

--- app/views/transactions.py
+++ app/views/transactions.py
@@ -28,24 +28,12 @@ def all_transactions(request):
         size = 50
         page = 1
 
-    total_data = [
-                     {
-                         "tx_hash": "0x4507a69d03d0dc145d5e7b47bc60f4796d99f594bd450749d689db6e8cebb157",
-                         "block": 6179847,
-                         "age": "2018-08-17 17:38:48",
-                         "from": "0xe32e3232e",
-                         "to": "0xe32e3232e",
-                         "value": 0,
-                         "tx_fee": 0.0000034
-                     }
-            ] * 500
-
     total_data = TransactionInfo.objects.all().order_by('-blockNumber')
 
     pag = Paginator(total_data, size)
     if page > pag.num_pages:
         page = 1
-    data = list(pag.page(page).object_list.values('hash', 'source', 'to', 'value', 'blockNumber'))
+    data = list(pag.page(page).object_list.values('hash', 'source', 'to', 'value', 'blockNumber', 'blockHash'))
     for item in data:
         number = item['blockNumber']
         item['time'] = stamp2datetime(Block.objects.get(number=number).timestamp)
