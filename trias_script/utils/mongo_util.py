dbe2fa17bc94e98a0b263debf862b2c6c8e365db
dasenlinCode <2845293434@qq.com>
2020-01-13 11:22:17 +0800

    修改浏览器获取bigchaindb数据的脚本，改为由mongodb中的bigchain表的_id字段的顺序，来确定区块的先后顺序。

---
+++ trias_script/utils/mongo_util.py
@@ -0,0 +1,95 @@
+from pymongo import MongoClient
+from utils.localconfig import JsonConfiguration
+from utils.logger import logger
+
+CONF = JsonConfiguration()
+
+
+def get_latest_block_id():
+    try:
+        conn = MongoClient(CONF.bdb_mongo_ip, CONF.bdb_mongo_port)
+        db = conn.get_database('octachain')
+        doc = db.get_collection('bigchain').find_one(sort=[('_id', -1)])
+        id = doc['_id']
+        return id
+    except Exception as e:
+        logger.error(e)
+        return None
+
+
+def get_first_block_hash1():
+    try:
+        conn = MongoClient(CONF.bdb_mongo_ip, CONF.bdb_mongo_port)
+        db = conn.get_database('octachain')
+        doc = db.get_collection('votes').find_one({"vote.is_block_valid":True}, sort=[('_id', 1)])
+        if doc:
+            hash = doc['vote']['voting_for_block']
+            return hash
+        else:
+            return None
+    except Exception as e:
+        logger.error(e)
+        return None
+
+def get_first_block_hash():
+    try:
+        conn = MongoClient(CONF.bdb_mongo_ip, CONF.bdb_mongo_port)
+        db = conn.get_database('octachain')
+        doc = db.get_collection('bigchain').find_one({"block.transactions.operation":'GENESIS'}, sort=[('_id', 1)])
+        if doc:
+            hash = doc['id']
+            return hash
+        else:
+            return None
+    except Exception as e:
+        logger.error(e)
+        return None
+
+def get_next_block_hash(previous_block_hash):
+    try:
+        conn = MongoClient(CONF.bdb_mongo_ip, CONF.bdb_mongo_port)
+        db = conn.get_database('octachain')
+        doc = db.get_collection('votes').find_one({"vote.is_block_valid":True, "vote.previous_block":previous_block_hash}, sort=[('_id', 1)])
+        if doc:
+            hash = doc['vote']['voting_for_block']
+            return hash
+        else:
+            return None
+    except Exception as e:
+        logger.error(e)
+        return None
+
+def get_block_with_hash(block_hash):
+    try:
+        conn = MongoClient(CONF.bdb_mongo_ip, CONF.bdb_mongo_port)
+        db = conn.get_database('octachain')
+        doc = db.get_collection('bigchain').find_one({"id":block_hash})
+        return doc
+    except Exception as e:
+        logger.error(e)
+        return None
+
+def get_asset_with_id(asset_id):
+    try:
+        conn = MongoClient(CONF.bdb_mongo_ip, CONF.bdb_mongo_port)
+        db = conn.get_database('octachain')
+        doc = db.get_collection('assets').find_one({"id":asset_id})
+        return doc
+    except Exception as e:
+        logger.error(e)
+        return None
+
+#first height is 1
+def get_block_by_height(height):
+    try:
+        conn = MongoClient(CONF.bdb_mongo_ip, CONF.bdb_mongo_port)
+        db = conn.get_database('octachain')
+        docs = list(db.get_collection('bigchain').find({}, sort=[('_id', 1)], limit=1, skip=height-1))
+        if len(docs) > 0:
+            return docs[0]
+        else:
+            return None
+    except Exception as e:
+        logger.error(e)
+        return None
+
