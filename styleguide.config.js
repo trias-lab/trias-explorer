32fbda6d916c8a2b69ac938fa72cabdc0c9a61ce
Evelynzzz <15104026362@163.com>
2020-02-21 17:22:49 +0800

    Configure react styleguidist

---
+++ styleguide.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+    webpackConfig: {
+        module: {
+            rules: [
+                // Babel loader will use your project’s babel.config.js
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                },
+                // Other loaders that are needed for your components
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.html$/,
+                    use: 'raw-loader'
+                },
+                {
+                    test: /\.(gif|png|jpg|jpeg)$/,
+                    use: 'url-loader?limit=8192'
+                },
+                {
+                    test: /\.(woff|woff2|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: 'url-loader?limit=81920'
+                },
+            ]
+        }
+    },
+    sections:[
+        {
+            name:"Page Components",
+            components: "html/components/**.js"
+        },
+        {
+            name:"UI Components",
+            components: "html/components/common/**.js"
+        },
+    ],
+    styleguideDir: "docs"
+}
